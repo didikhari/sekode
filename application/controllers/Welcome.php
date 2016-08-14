59a3d608fdeabf2965794fd81a592dfc5d440a83
klatencoder <didikhari38@gmail.com>
2016-08-14 11:41:32 +0700

    appy template

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,10 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+        $data = array(
+                'title' => 'DidikH'
+            );
+        $this->load_template('login', 'form-login', $data);
 	}
+    
 }
